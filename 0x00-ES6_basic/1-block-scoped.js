d7d9a46f668562e6ef8974e4fc9dbaf9c9d91edb
dohoudaniel <dohoudanielfavour@gmail.com>
2024-05-31 17:02:32 +0100

    Modify variables

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    var task = this.task;
-    var task2 = this.task2;
+    task = this.task;
+    task2 = this.task2;
   }
 
   return [task, task2];
