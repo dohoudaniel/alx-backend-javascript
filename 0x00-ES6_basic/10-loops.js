0bdfbceafed8ac9d8eefbe80daeacbdbd301e477
dohoudaniel <dohoudanielfavour@gmail.com>
2024-06-24 18:21:27 +0100

    Appending strings using loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,9 @@
+/* eslint-disable guard-for-in */
+/* eslint-disable no-param-reassign */
 export default function appendToEachArrayValue(array, appendString) {
-  for (const idx of array) {
-    const value = array[idx];
-    array[idx] = appendString + value;
+  const finalArray = [];
+  for (const item of array) {
+    finalArray.push(appendString + item);
   }
 
   return array;
