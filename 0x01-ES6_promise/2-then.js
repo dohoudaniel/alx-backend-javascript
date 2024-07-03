5f56a224195f0ef73a351367205d597fc813aa84
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-03 17:57:15 +0100

    Remove Promise declaration and calling 'then' methods

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({
-        status: 200,
-        body: success
-      }).then(console.log('Got a response from the API'));
-    } else {
-      reject(new Error());
-    }
-  });
+  promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => { console.log('Got a response from the API'); });
 }
