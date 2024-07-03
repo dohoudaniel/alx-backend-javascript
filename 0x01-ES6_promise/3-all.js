d3c6f8b80a2fee7586b16da84a7cc7b88aa072ee
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-03 18:12:56 +0100

    Handling multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,6 +3,11 @@ import uploadPhoto from './utils.js';
 import createUser from './utils.js';
 
 export default function handleProfileSignup() {
-  .then(() => { console.log(`${uploadPhoto.body} ${createUser.firstName} ${createUser.lastName}`); })
-  .catch(() => { console.log('Signup system offline'); })
+  const photo = uploadPhoto();
+  const user = createUser();
+  //
+  // Fulfilling multiple promises using the Promise.all() method
+  return new Promise.all([photo, user])
+    .then((promisesFulfilled) => { console.log(`${promisesFulfilled[0].body} ${promisesFulfilled[1].firstName} ${promisesFulfilled[1].lastName}`); })
+    .catch(() => { console.log('Signup system offline'); })
 }
