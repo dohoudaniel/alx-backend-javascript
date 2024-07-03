af367260ffaabacc63486e1fed75810f058c41b2
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-03 17:29:02 +0100

    Return an empty Error object

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,12 @@
+export default function handleResponseFromAPI(promise) {
+  return new Promise((resolve, reject) {
+    if (promise) {
+      resolve({
+        status: 200,
+        body: success
+      }).then(console.log(Got a response from the API));
+    } else {
+        reject(new Error());
+    }
+  });
+}
