b3dc916a592a2885b02bba95026c3522150c0aeb
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-03 18:16:49 +0100

    Delete 'new' statement

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -7,7 +7,7 @@ export default function handleProfileSignup() {
   const user = createUser();
   //
   // Fulfilling multiple promises using the Promise.all() method
-  return new Promise.all([photo, user])
+  return Promise.all([photo, user])
     .then((promisesFulfilled) => { console.log(`${promisesFulfilled[0].body} ${promisesFulfilled[1].firstName} ${promisesFulfilled[1].lastName}`); })
     .catch(() => { console.log('Signup system offline'); });
 }
