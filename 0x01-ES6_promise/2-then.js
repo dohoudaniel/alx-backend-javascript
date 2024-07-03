0f2b0c9535aa43e1beadf7fc08224ab46ed36f6d
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-03 17:30:58 +0100

    Add arrow function

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,12 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) {
+  return new Promise((resolve, reject) => {
     if (promise) {
       resolve({
         status: 200,
         body: success
-      }).then(console.log(Got a response from the API));
+      }).then(console.log('Got a response from the API'));
     } else {
-        reject(new Error());
+      reject(new Error());
     }
   });
 }
