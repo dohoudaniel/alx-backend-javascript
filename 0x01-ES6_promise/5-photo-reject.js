fa2f5a7c189e29af9979197f3e627d1141f8e07a
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-04 00:09:02 +0100

    Add resolve call

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -2,6 +2,8 @@ export default function uploadPhoto(filename) {
   return new Promise((resolve, reject) => {
     if (!filename) {
       reject(`${filename} cannot be processed`);
+    } else {
+      resolve();
     }
   });
 }
