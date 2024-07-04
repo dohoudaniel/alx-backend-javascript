42b8434575c14c504b68f29ec95d14c2abe65a95
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-04 17:35:17 +0100

    Handling and resolving multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,9 +2,23 @@ import signUpUser from './4-user-promise.';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const newUser = signUpUser();
-  const newPhoto = uploadPhoto();
+  const newUser = signUpUser(firstName, lastName);
+  const newPhoto = uploadPhoto(fileName);
 
-  return Promise.all([newUser, newPhoto])
-    .then(() => { [ {} ] })
+  return Promise.allSettled([newUser, newPhoto])
+    .then((values) => {
+      // Initialize an empty array to be returned
+      const result = [];
+      values.forEach((element) => {
+        if (element.status === 'fulfilled') {
+          // Appending the resolution result to the `result` array:
+          // 'status: status_of_the_promise, value: value returned by the Promise'
+          result.push({ status: element.status, value: element.value });
+        } else {
+          // Appending the rejection result to the `result` array:
+          // 'status: status_of_the_promise, value: error returned by the Promise'
+          result.push({ status: element.status, value: `${element.reason}` });
+        }
+      });
+    });
 }
