ffc2d945b2117e5ca9baa3b60c3c7594ac2bb784
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-10 00:27:27 +0100

    Returning new objects of a class using constructors

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -7,7 +7,11 @@ export default class Car {
 
   // This method returns a new object of the class
   cloneCar() {
-    return new Car(this._brand, this._motor, this._color);
-    // return new this.constructor(this._brand, this._motor, this._color);
+    // The line of code below does not work because:
+    // - it would always create an instance of the Car class, and
+    // this can be very harmful in a situation where the class is extended
+    // and in terms of subclassing too.
+    // return new Car(this._brand, this._motor, this._color);
+    return new this.constructor(this._brand, this._motor, this._color);
   }
 }
