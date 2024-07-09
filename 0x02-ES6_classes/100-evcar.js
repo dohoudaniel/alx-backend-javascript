557a775b13a4c6f2f4d455304a2c7dc90e7f8106
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-10 00:31:17 +0100

    A class extending another class with privacy reasons

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,13 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    // This method was used for privacy reasons
+    return new Car();
+  }
+}
