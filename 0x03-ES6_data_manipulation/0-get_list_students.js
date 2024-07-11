87550cfb5592e9067bfc09c25f25e14232a13e01
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-11 16:53:37 +0100

    Rewrite how the aarray of objects is returned

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,18 +1,19 @@
 export default function getListStudents() {
-  const firstObject = {
-    id: 1,
-    firstName: 'Guillaume',
-    location: 'San Francisco',
-  };
-  const secondObject = {
-    id: 2,
-    firstName: 'James',
-    location: 'Columbia',
-  };
-  const thirdObject = {
-    id: 3,
-    firstName: 'Serena',
-    location: 'San Francisco',
-  };
-  return [firstObject, secondObject, thirdObject];
+  return [
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 3,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    },
+  ];
 }
