d6485f3969a3c538dda76a2d7ab9d3e719021a64
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-11 16:48:41 +0100

    Return an array of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,18 @@
+export default function getListStudents() {
+  const firstObject = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
+  const secondObject = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+  const thirdObject = {
+    id: 3,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+  return [firstObject, secondObject, thirdObject];
+}
