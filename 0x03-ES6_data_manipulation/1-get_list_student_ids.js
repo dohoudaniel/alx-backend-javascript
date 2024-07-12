e484e1543f85e3272e952e3d9e9606c368abcc3c
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-12 19:34:26 +0100

    Add parentheses

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,6 +2,6 @@ export default function getListStudentIds(argument) {
   if (!Array.isArray(argument)) {
     return [];
   }
-  const myArray = argument.map(item => (item.id));
+  const myArray = argument.map((item) => (item.id));
   return myArray;
 }
