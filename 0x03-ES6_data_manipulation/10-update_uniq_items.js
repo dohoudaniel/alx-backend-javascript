5944125fe2bb7b32d102fc3c2ea859dbaa0236d2
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-12 23:55:45 +0100

    Update the Map's values uaing the keys

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  const keys = Array.from(map.keys());
+  for (const key of keys) {
+    if (map.get(key) === 1) {
+      map.set(key, 100);
+    }
+  }
+}
