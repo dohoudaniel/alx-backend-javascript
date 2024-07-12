b6334748ec15c49c65f2a394a511c6bd0e28e85f
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-12 23:37:37 +0100

    Returning truncated strings

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,15 @@
+export default function cleanSet(set, startString) {
+  // Check if the startString is empty and return an empty string
+  if (!startString || startString.length === 0) {
+    return '';
+  }
+  const cleanSet = [];
+  for (const value of set) {
+    // Checking if value starts with startString
+    if (value.startsWith(startString)) {
+      cleanSet.push(value.subString(startString.length));
+    }
+  }
+  // Join the final array with a hyphen and a resulting string
+  return cleanSet.join('-');
+}
