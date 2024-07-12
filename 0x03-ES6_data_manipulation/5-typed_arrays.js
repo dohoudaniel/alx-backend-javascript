22bc8ce5833357047ef9f6c0d7a7c54610a8a033
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-12 22:57:56 +0100

    Add comments

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,15 @@
 export default function createInt8TypedArray(length, position, value) {
+  // Create an ArrayBuffer of size length
+  // An ArrayBuffer is a fixed-length raw binary data buffer.
   const buffer = new ArrayBuffer(length);
+  // Handling errors
   if (position >= length || position < 0) {
     throw new Error('Position outside range');
   }
+  // Create a DataView to interact with, and manipulate buffer's raw binary data
   const view = new DataView(buffer);
+  // Setting the Int8 value at position
   view.setInt8(position, value);
+  // Returning the view
   return view;
 }
