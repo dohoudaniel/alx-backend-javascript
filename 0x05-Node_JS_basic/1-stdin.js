aa3b9d7baea5d8af8044ded9e7dc4e1d520386b7
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-27 19:38:36 +0100

    Update script

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,10 +6,22 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 //
 // A boolean property checking if the standard input
 // is coming from an interactive shell session
-process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data.toString()}`);
-  process.exit();
-}
-process.on('exit', () => {
+if (process.stdin.isTTY) {
+  // Convert the input buffer to a string and
+  // writes the input back to the terminal
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  // Handling an event whereby the input is not
+  // coming from a terminal
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  // What the 'exit' function does when it is called
+  process.on('exit', () => {
     process.stdout.write('This important software is now closing\n');
   });
+}
