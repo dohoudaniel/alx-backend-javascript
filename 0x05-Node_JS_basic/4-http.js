843571271e2933fb47a5dd27b4f34c36bb2c46f7
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-28 00:52:39 +0100

    Change import statement, and call correct method to create the server ('createServer').

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,10 +1,10 @@
 // Creating a small HTTP server using the NodeJS HTTP module
-const createServer = require('http');
+const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const app = createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
