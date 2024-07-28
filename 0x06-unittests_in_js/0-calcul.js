926146c150f8277faa6213779e6eca5e035336d4
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-28 10:09:57 +0100

    Delete directory

--- 0x06-unittests_in_js/0-calcul.js
+++
@@ -1,6 +0,0 @@
-const { round } = Math;
-function calculateNumber(a, b) {
-  return round(a) + round(b);
-}
-
-module.exports = calculateNumber;
--- 0x06-unittests_in_js/6-payment_token.js
+++
@@ -1,8 +0,0 @@
-function getPaymentTokenFromAPI(success) {
-    if (success) {
-        return new Promise((resolve, reject) => {
-            resolve({ data: 'Successful response from the API' });
-          });
-    };
-}
-module.exports = getPaymentTokenFromAPI
